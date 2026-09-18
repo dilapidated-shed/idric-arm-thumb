50f294b6c50ee24bb77894028859df5715d61583
i <isomorphisms@sdf.org>
2026-09-18 12:28:05 -0400

    Use canonical preflight in current-head CI

--- tests/dex/run-current-head-ci.sh
+++ tests/dex/run-current-head-ci.sh
@@ -86,7 +86,7 @@ run_stage make -C "$idric_support_root" install-api IDRIS2_BOOT="$compiler"
 passed="$passed compiler_api_install"
 
 current_stage=backend_build
-run_stage make -C "$repo_root" check driver IDRIC="$compiler" IDRIC_REPO="$idric_repo" IDRIC_COMPILER_REF="$compiler_ref"
+run_stage make -C "$repo_root" preflight IDRIC="$compiler" IDRIC_REPO="$idric_repo" IDRIC_COMPILER_REF="$compiler_ref"
 passed="$passed backend_build"
 
 current_stage=wegert_direct_dex
